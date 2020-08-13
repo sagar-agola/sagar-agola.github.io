63aae1bace9c546beb00dccea8d4cfb89e9f2f75
sagar-agola <n3o735@gmail.com>
2020-08-13 22:47:24 +0530

    fixed script theme issue

--- script.js
+++ script.js
@@ -17,19 +17,19 @@ for (var i = 0; themeDots.length > i; i++) {
 
 function setTheme(mode) {
 	if (mode == 'light') {
-		document.getElementById('theme-style').href = 'default.css';
+		document.getElementById('theme-style').href = 'style/default.css';
 	}
 
 	if (mode == 'blue') {
-		document.getElementById('theme-style').href = 'blue.css';
+		document.getElementById('theme-style').href = 'style/blue.css';
 	}
 
 	if (mode == 'green') {
-		document.getElementById('theme-style').href = 'green.css';
+		document.getElementById('theme-style').href = 'style/green.css';
 	}
 
 	if (mode == 'purple') {
-		document.getElementById('theme-style').href = 'purple.css';
+		document.getElementById('theme-style').href = 'style/purple.css';
 	}
 
 	localStorage.setItem('theme', mode);
